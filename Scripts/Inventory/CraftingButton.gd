6795edffc38302dab4ce13d88ba314e01bbaf3fb
Yuna <rushikeshingale2001@gmail.com>
2025-11-21 18:24:38 +0530

    Added Crafting Buttons And UI With Basic Logic

---
+++ Scripts/Inventory/CraftingButton.gd
@@ -0,0 +1,9 @@
+extends TextureRect
+
+@onready var CraftIcon: TextureRect = $MarginContainer/Icon
+
+var item_key
+
+func set_item_key(_item_key) -> void:
+	item_key = _item_key
+	CraftIcon.texture = ItemConfig.get_item_resource(item_key).icon
